4bab7292400063388f07571de5a03eb2a9a112d2
behnam <b.hajian@gmail.com>
2026-04-24 08:56:20 -0400

    fixes issues

--- OCP/k8s/slinky/install.sh
+++ OCP/k8s/slinky/install.sh
@@ -9,21 +9,21 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 
-helm install cert-manager jetstack/cert-manager \
+helm upgrade --install cert-manager jetstack/cert-manager \
   --namespace cert-manager --create-namespace --set crds.enabled=true
 
-helm install prometheus prometheus-community/kube-prometheus-stack \
+helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \
   --namespace prometheus --create-namespace --set installCRDs=true
 
-helm install slurm-operator-crds oci://ghcr.io/slinkyproject/charts/slurm-operator-crds \
+helm upgrade --install slurm-operator-crds oci://ghcr.io/slinkyproject/charts/slurm-operator-crds \
   --version "${SLINKY_VERSION}"
 
-helm install slurm-operator oci://ghcr.io/slinkyproject/charts/slurm-operator \
+helm upgrade --install slurm-operator oci://ghcr.io/slinkyproject/charts/slurm-operator \
   --version "${SLINKY_VERSION}" \
   --namespace slinky --create-namespace \
   --values k8s/slinky/values-operator.yaml
 
-helm install slurm oci://ghcr.io/slinkyproject/charts/slurm \
+helm upgrade --install slurm oci://ghcr.io/slinkyproject/charts/slurm \
   --version "${SLINKY_VERSION}" \
   --namespace slurm --create-namespace \
   --values k8s/slinky/values-slurm.yaml
